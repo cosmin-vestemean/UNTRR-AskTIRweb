5d4c986be3fc3fcac522f5f2cb1df34e794079da
Cosmin Veștemean <cosmin.ve@gmail.com>
2022-04-27 15:22:40 +0300

    Update getDetails.sql

--- API/getDetails.sql
+++ API/getDetails.sql
@@ -1,30 +1,15 @@
-SELECT (
-		SELECT seriesnum + 1
-		FROM seriesnum
-		WHERE fiscprd = year(getdate())
-			AND series = 3110
-		) seriesnum
-	,(
-		SELECT dateadd(dd, 70, '{asktirissuedate}')
-		) date02
-	,(
-		SELECT branch
-		FROM branch
-		WHERE CCCASKTIRID = {asktirbranch}
-		) branch
-	,(
-		SELECT series
-		FROM series
-		WHERE fprms = 3001
-			AND sosource = 1351
-			AND branch = (
-				SELECT branch
-				FROM branch
-				WHERE cccasktirid = {asktirbranch}
-				)
-		) series
-	,(
-		SELECT trdr
-		FROM trdr
-		WHERE code1 = '{asktirhauliercode}'
-		) trdr
+select
+	(select branch
+	from branch
+	where CCCASKTIRID={asktirbranch}) branch,
+	(select branch
+	from branch
+	where CCCASKTIRID={asktirbranch1}) branch1,
+	(select series
+	from series
+	where fprms=3001 and sosource=1351 and branch = (select branch
+		from branch
+		where cccasktirid={asktirbranch})) series,
+	(select trdr
+	from trdr
+	where code1 = '{asktirhauliercode}') trdr
